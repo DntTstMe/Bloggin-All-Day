20855b9e54050983373d02a7351246c7260ba92a
Alex Tester <lex.tester93@gmail.com>
2023-10-23 10:41:13 -0400

    Added styling details in  css folder

--- models/index.js
+++ models/index.js
@@ -1,12 +1,16 @@
 // Imports
-const { User, BlogPost, Comment } = require("./models");
+const User = require("./user");
+const BlogPost = require("./blogPost");
+const Comment = require("./comment");
 
+// Define associations in an array
 const associations = [
   { source: User, target: BlogPost, foreignKey: "user_id" },
   { source: User, target: Comment, foreignKey: "user_id" },
   { source: Comment, target: BlogPost, foreignKey: "blogPost_id" },
 ];
 
+// Set up associations using a loop
 associations.forEach(({ source, target, foreignKey, onDelete }) => {
   source.hasMany(target, { foreignKey, onDelete: "CASCADE" });
   target.belongsTo(source, { foreignKey });
@@ -14,3 +18,4 @@ associations.forEach(({ source, target, foreignKey, onDelete }) => {
 
 // Export
 module.exports = { User, BlogPost, Comment };
+
